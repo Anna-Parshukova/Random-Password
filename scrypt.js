d1b65c504789ef4aaea1d61792b7a8fb5cd79afc
Anna Parshukova <annpaarshukova041@gmail.com>
2025-02-18 23:06:45 +0200

    summary

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+// const passwordBox = document.getElementById('password');
+// const lenght = 12;
+// const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+// const numbers = '0123456789';
+// const symbol = '!@#$%^&*';
+
+// const allChars = upperCase + lowerCase + numbers + symbol;
+
+// function createPassword() {
+//     let password = "";
+//     password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//     password += numbers[Math.floor(Math.random() * numbers.length)];
+//     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+//     while (password.length < lenght) {
+//         password += allChars[Math.floor(Math.random() * allChars.length)];
+//     }
+//     passwordBox.value = password;
+// }
